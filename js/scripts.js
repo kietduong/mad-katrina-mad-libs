4fba18624bc56e2eb1e69821905e7e1b604c1c92
Kiet Duong and Katrina Hirsch <kiet@>
2016-08-15 13:47:32 -0700

    consolidated javascript with looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,11 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var adjective1Input = $("input#adjective1").val();
-    var adjective2Input = $("input#adjective2").val();
-    var verbing1Input = $("input#verb-ing1").val();
-    var verbing2Input = $("input#verb-ing2").val();
-    var adjective3Input = $("input#adjective3").val();
-    var adjective4Input = $("input#adjective4").val();
-    var nounInput = $("input#noun").val();
+    var blanks = ["adjective1", "adjective2", "verb-ing1", "verb-ing2", "adjective3", "adjective4", "noun"];
 
-    $(".adjective1").text(adjective1Input);
-    $(".adjective2").text(adjective2Input);
-    $(".verb-ing1").text(verbing1Input);
-    $(".verb-ing2").text(verbing2Input);
-    $(".adjective3").text(adjective3Input);
-    $(".adjective4").text(adjective4Input);
-    $(".noun").text(nounInput);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
 
